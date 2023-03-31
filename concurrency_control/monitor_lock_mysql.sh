ef3ad24b4e4d275532c9714ef35511747eec0ff1
Lee Bohyun <55489991+LeeBohyun@users.noreply.github.com>
2023-03-31 11:31:01 +0900

    Create monitor_lock_mysql.sh

---
+++ concurrency_control/monitor_lock_mysql.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+mysql_dir="/home/lbh/mysql-5.6.26"
+result_dir="/home/lbh/result/lock"
+
+
+end=$((SECONDS+1800))
+
+# Write mutex information to file every second
+while [ $SECONDS -lt $end ]
+do
+    # Get mutex information and write it to the result file
+    ${mysql_dir}/bin/mysql -uroot -pxxxxxx \
+        -e "SELECT EVENT_NAME, COUNT_STAR, SUM_TIMER_WAIT/1000000000 SUM_TIMER_WAIT_MS, \
+            AVG_TIMER_WAIT/1000000000 AVG_TIMER_WAIT_MS \
+            FROM performance_schema.events_waits_summary_global_by_event_name \
+            ORDER BY SUM_TIMER_WAIT_MS DESC;" >> ${result_dir}/wait_events.out
+
+    
+        ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
+                -e "SHOW STATUS LIKE '%row_lock%';" >> ${result_dir}/row_lock_wait.out
+
+
+        ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
+            -e "SELECT EVENT_NAME, COUNT_STAR, SUM_TIMER_WAIT/1000000000 SUM_TIMER_WAIT_MS, \
+            AVG_TIMER_WAIT/1000000000 AVG_TIMER_WAIT_MS \
+            FROM performance_schema.events_waits_summary_global_by_event_name \
+            WHERE SUM_TIMER_WAIT > 0 AND \
+            EVENT_NAME LIKE 'wait/io/file/%' \
+            ORDER BY SUM_TIMER_WAIT_MS DESC;" >> ${result_dir}/io_wait.out
+
+        ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
+            -e "SELECT * \
+            FROM performance_schema.events_transactions_summary_global_by_event_name \
+            LIMIT 1;" >> ${result_dir}/transaction_summary.out
+
+        ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
+            -e "SELECT * FROM INFORMATION_SCHEMA.INNODB_LOCK_WAITS;" >> ${result_dir}/transaction_lock_wait.out
+
+        ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
+            -e "SELECT EVENT_NAME, COUNT_STAR, SUM_TIMER_WAIT/1000000000 SUM_TIMER_WAIT_MS, \
+            AVG_TIMER_WAIT/1000000000 AVG_TIMER_WAIT_MS \
+            FROM performance_schema.table_lock_waits_summary_by_table \
+            WHERE SUM_TIMER_WAIT > 0 \
+            ORDER BY SUM_TIMER_WAIT_MS DESC;" >> ${result_dir}/table_lock_waits.out
+
+        ${mysql_dir}./bin/mysql -uroot -pxxxxxx\
+            -e "SELECT * FROM INFORMATION_SCHEMA.INNODB_TRX;" >> ${result_dir}/transaction.out
+
+        ${mysql_dir}./bin/mysql -uroot -pxxxxxx\
+            -e "SELECT * FROM INFORMATION_SCHEMA.INNODB_LOCK_WAITS;" >> ${result_dir}/transaction.out
+
+
+
+
+    sleep 0.1
+done
