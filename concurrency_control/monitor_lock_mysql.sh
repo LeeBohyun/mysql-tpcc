2e4b434f94ccc114f06a9211594cc46dcb698a94
Lee Bohyun <55489991+LeeBohyun@users.noreply.github.com>
2023-03-31 13:14:24 +0900

    Update monitor_lock_mysql.sh

--- concurrency_control/monitor_lock_mysql.sh
+++ concurrency_control/monitor_lock_mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql_dir="/home/lbh/mysql-5.6.26"
+mysql_dir="/home/lbh/mysql-5.7.24"
 result_dir="/home/lbh/result/lock"
 
 
@@ -11,8 +11,8 @@ while [ $SECONDS -lt $end ]
 do
     # Get mutex information and write it to the result file
     ${mysql_dir}/bin/mysql -uroot -pxxxxxx \
-        -e "SELECT EVENT_NAME, COUNT_STAR, SUM_TIMER_WAIT/1000000000 SUM_TIMER_WAIT_MS, \
-            AVG_TIMER_WAIT/1000000000 AVG_TIMER_WAIT_MS \
+        -e "SELECT EVENT_NAME, COUNT_STAR, SUM_TIMER_WAIT SUM_TIMER_WAIT_MS, \
+            AVG_TIMER_WAIT AVG_TIMER_WAIT_MS \
             FROM performance_schema.events_waits_summary_global_by_event_name \
             ORDER BY SUM_TIMER_WAIT_MS DESC;" >> ${result_dir}/wait_events.out
 
@@ -38,20 +38,21 @@ do
             -e "SELECT * FROM INFORMATION_SCHEMA.INNODB_LOCK_WAITS;" >> ${result_dir}/transaction_lock_wait.out
 
         ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
-            -e "SELECT EVENT_NAME, COUNT_STAR, SUM_TIMER_WAIT/1000000000 SUM_TIMER_WAIT_MS, \
+            -e "SELECT COUNT_STAR, SUM_TIMER_WAIT/1000000000 SUM_TIMER_WAIT_MS, \
             AVG_TIMER_WAIT/1000000000 AVG_TIMER_WAIT_MS \
             FROM performance_schema.table_lock_waits_summary_by_table \
             WHERE SUM_TIMER_WAIT > 0 \
             ORDER BY SUM_TIMER_WAIT_MS DESC;" >> ${result_dir}/table_lock_waits.out
 
-        ${mysql_dir}./bin/mysql -uroot -pxxxxxx\
+        ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
             -e "SELECT * FROM INFORMATION_SCHEMA.INNODB_TRX;" >> ${result_dir}/transaction.out
 
-        ${mysql_dir}./bin/mysql -uroot -pxxxxxx\
-            -e "SELECT * FROM INFORMATION_SCHEMA.INNODB_LOCK_WAITS;" >> ${result_dir}/transaction.out
+
+        ${mysql_dir}/bin/mysql -uroot -pxxxxxx\
+            -e "SELECT * FROM performance_schema.events_waits_current ;" >> ${result_dir}/transaction_mutex.out
 
 
 
 
-    sleep 0.1
+    sleep 0.05
 done
